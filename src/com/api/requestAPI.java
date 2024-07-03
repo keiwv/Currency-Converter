df6d4b022f6140b3ecbbba3c583b8ecdf5f4ed9b
keiwv <141773283+keiwv@users.noreply.github.com>
2024-07-02 19:49:08 -0700

    Added http request and client

--- src/com/api/requestAPI.java
+++ src/com/api/requestAPI.java
@@ -1,2 +1,17 @@
-package com.api;public class requestAPI {
+package com.api;
+
+import java.net.URI;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpClient;
+
+public class requestAPI {
+    String URL = "";
+
+    HttpClient client = HttpClient.newHttpClient();
+    HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create(URL))
+            .build();
+
+    
 }
