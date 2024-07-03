35bc56bd7eb270aaaeb8a493b385b4a23f23f25c
keiwv <141773283+keiwv@users.noreply.github.com>
2024-07-02 19:30:23 -0700

    Added menu classes and still needed to add logic for each case

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import com.menu.Menu;
+
+public class Main {
+    public static void main(String[] args) {
+        int option = -1;
+        Menu menu = new Menu();
+
+        while(option != 7)
+        {
+            option = menu.getOption();
+
+            switch (option)
+            {
+                case 1:
+
+            }
+        }
+    }
+}
---
+++ src/com/menu/Menu.java
@@ -0,0 +1,34 @@
+package com.menu;
+
+
+import java.util.Scanner;
+
+public class Menu {
+
+    protected int option;
+
+    public int getOption() {
+        option = -1;
+        Scanner sc = new Scanner(System.in);
+        while(option <= 0 || option >= 8)
+        {
+            options();
+            option = sc.nextInt();
+        }
+        return option;
+    }
+
+    private void options() {
+        System.out.println("*************************************");
+        System.out.println("Sea bienvenido/a al conversor de monedas!\n\n");
+        System.out.println("1) Dólar =>> Peso Argentino");
+        System.out.println("2) Peso Argentino =>> Dólar");
+        System.out.println("3) Dólar =>> Real brasileño");
+        System.out.println("4) Real brasileño =>> Dólar");
+        System.out.println("5) Dólar =>> peso colombiano");
+        System.out.println("6) Peso colombiano =>> Dólar");
+        System.out.println("7) Salir");
+        System.out.println("*************************************");
+        System.out.println("Por favor, selecciona una opción: ");
+    }
+}
