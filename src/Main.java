170179dec5ef824a89e4702d827770aa09f2220b
keiwv <141773283+keiwv@users.noreply.github.com>
2024-07-05 12:53:07 -0700

    finished

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,50 @@
 import com.menu.Menu;
 import com.api.requestAPI;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         int option = -1;
         Menu menu = new Menu();
         requestAPI api = new requestAPI();
         Double total = 0.0;
+        float amount = 0.0f;
+        Scanner scanner = new Scanner(System.in);
 
         while (option != 7) {
 
             option = menu.getOption();
 
+            if (option >= 1 && option <= 6) {
+                System.out.println("Ingrese la cantidad a convertir: ");
+                amount = scanner.nextFloat();
+            }
+
             switch (option) {
                 case 1:
-                    total = api.getCurrency("USD", "ARS", 10);
-                    System.out.println("10 USD en Pesos Argentinos: " + total);
+                    total = api.getCurrency("USD", "ARS", amount);
+                    System.out.println(amount + " dólares son " + total + " pesos argentinos");
                     break;
                 case 2:
-                    total = api.getCurrency("ARS", "USD", 10);
-                    System.out.println("10 Pesos Argentinos en USD: " + total);
+                    total = api.getCurrency("ARS", "USD", amount);
+                    System.out.println(amount + " pesos argentinos son " + total + " dólares");
                     break;
                 case 3:
-                    total = api.getCurrency("USD", "BRL", 10);
-                    System.out.println("10 USD en Reales Brasileños: " + total);
+                    total = api.getCurrency("USD", "BRL", amount);
+                    System.out.println(amount + " dólares son " + total + " reales brasileños");
                     break;
                 case 4:
-                    total = api.getCurrency("BRL", "USD", 10);
-                    System.out.println("10 Reales Brasileños en USD: " + total);
+                    total = api.getCurrency("BRL", "USD", amount);
+                    System.out.println(amount + " reales brasileños son " + total + " dólares");
                     break;
                 case 5:
-                    total = api.getCurrency("USD", "COP", 10);
-                    System.out.println("10 USD en Pesos Colombianos: " + total);
+                    total = api.getCurrency("USD", "COP", amount);
+                    System.out.println(amount + " dólares son " + total + " pesos colombianos");
                     break;
                 case 6:
-                    total = api.getCurrency("COP", "USD", 10);
-                    System.out.println("10 Pesos Colombianos en USD: " + total);
+                    total = api.getCurrency("COP", "USD", amount);
+                    System.out.println(amount + " pesos colombianos son " + total + " dólares");
                     break;
                 case 7:
                     System.out.println("Saliendo del programa. ¡Gracias por usar el conversor de monedas!");
